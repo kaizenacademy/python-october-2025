579e3dc1ea7096361ca92a2b78daf69579e2f669
kaizenacademy <kaizen.techacademy@gmail.com>
2025-12-16 01:58:45 +0000

    added class1 files

---
+++ class1/set.py
@@ -0,0 +1,5 @@
+food = ["burger", "pizza", "taco", "burger", "taco"]
+print(food)
+
+food = {"burger", "pizza", "taco", "burger", "taco"}
+print(food)
---
+++ class1/var.py
@@ -0,0 +1,8 @@
+name = "Tim"
+print(f"Hello {name}")
+
+year=3
+
+print(f"in {year} years {year*365} days")
+print(f"in {year} years {year*52} weeks")
+
